a8a69645aa0f46ad7d241bc03234709d2a77f630
Bruna Osti <brunaosti@alunos.utfpr.edu.br>
2020-06-09 12:40:04 -0300

    leitura do teclado add pacote simulação de ataque attack.pcap

--- exp05/dissector.py
+++ exp05/dissector.py
@@ -1,10 +1,27 @@
+#!/usr/bin/env python3
+"""Can be executed by:
+   chmod +x dissector.py
+   ./dissector -r package.pcap
+"""
 import sys
 
-
-
-
-
-
-if __init__ == "__main__":
-    param = sys.argv[1:]
+def main(archive):
     pass
+
+
+
+if __name__ == "__main__":
+    param = sys.argv[1:]
+    
+    if len(param):
+        if param[0] == '-r':
+            archive = param[1]
+            main(archive)
+
+        elif param[0] == '--help':
+            print('-r package.pcap')
+            sys.exit(0)
+
+    else:
+        print("Should read as argument a file in pcap format but none was given!")
+        sys.exit(0)
